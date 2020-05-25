9b5cb156b7c507d53e164db96e1772e04481706a
unknown <suporte@STFBHZC030941-L.stefanini.dom>
2020-05-25 19:22:53 -0300

    firt commit

---
+++ api/api.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import pre_process.pre_processamento as pp
+import os
+app = Flask(__name__)
+
+
+@app.route('/stf', methods=['POST'])
+def quebrarCaptcha():
+    try:
+        data = request.get_json()
+        img = pp.quebrar_captcha(data["captcha"])
+        if img is not None:
+            return {"captcha": pp.previsao(img)}, 200
+        else:
+            return {"captcha": "AAAA"}, 200
+    except:
+        return {
+                   "captcha": "Não foi possível completar a requisição, verifique se o parâmetro captcha foi informado e o base64 é válido"}, 409
+
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 5000)
+    app.run(debug=False, port=port, host='0.0.0.0')
+
---
+++ pre_process/pre_processamento.py
@@ -0,0 +1,55 @@
+import base64
+import numpy as np
+import cv2
+from keras.models import load_model
+import os
+import pickle
+
+
+def cortar_imagem(img):
+    try:
+        arrayimg = []
+        cont = 0
+        nparr = np.fromstring(base64.decodebytes(img.encode()), np.uint8)
+        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        for cont in range(4):
+            y = 0
+            x = 20 * cont
+            h = 40
+            w = 20
+            crop = image[y:y + h, x:x + w]
+            lower = np.array([0, 180, 0])
+            upper = np.array([100, 180, 100])
+            mask = cv2.inRange(crop, lower, upper)
+            res = cv2.bitwise_and(crop, crop, mask=mask)
+            res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+            res = cv2.threshold(res, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)[1]
+            arrayimg.append(res)
+        return arrayimg
+    except:
+        return None
+
+
+def quebrar_captcha(base64):
+    img = cortar_imagem(base64)
+    if img is not None:
+        return img
+    else:
+        return None
+
+
+def previsao(img):
+    captcha = ''
+    global model
+    model = load_model('model\captcha_model.hdf5')
+    model._make_predict_function()
+    with open('model\model_labels.dat', "rb") as f:
+        lb = pickle.load(f)
+    if img is not None:
+        for imgP in img:
+            imgP = np.stack((imgP,) * 3, axis=-1)
+            imgP = cv2.resize(imgP, (50, 50))
+            imgP = imgP.reshape(-1, 50, 50, 3)
+            prediction = model.predict(imgP)
+            captcha = captcha + lb.classes_[np.argmax(prediction)][0]
+    return captcha
