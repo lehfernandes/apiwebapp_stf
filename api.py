0c09dac2a127c97420fa0b9c7f10fd34970aafd3
unknown <suporte@STFBHZC030941-L.stefanini.dom>
2020-05-25 22:03:52 -0300

    server install

--- api.py
+++ api.py
@@ -1,24 +1,27 @@
 from flask import Flask, request
+from waitress import serve
 import pre_process.pre_processamento as pp
 import os
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/stf', methods=['POST'])
 def quebrarCaptcha():
-    #try:
+    try:
         data = request.get_json()
         img = pp.quebrar_captcha(data["captcha"])
         if img is not None:
             return {"captcha": pp.previsao(img)}, 200
         else:
             return {"captcha": "AAAA"}, 200
-    #except:
-        #return {
-                   #"captcha": "Não foi possível completar a requisição, verifique se o parâmetro captcha foi informado e o base64 é válido"}, 409
+    except:
+        return {
+                   "captcha": "Não foi possível completar a requisição, verifique se o parâmetro captcha foi informado e o base64 é válido"}, 409
 
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
-    app.run(debug=True, port=port, host='0.0.0.0')
+    #app.run(debug=True, port=port, host='0.0.0.0')
+    serve(app, port=port, host='0.0.0.0')
 
