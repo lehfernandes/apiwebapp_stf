615d1035ce2f0c6f636a468f7f974818782eb1ed
Leandro Fernandes <64205448+lehfernandes@users.noreply.github.com>
2020-06-03 23:48:32 -0300

    Update api.py

--- api.py
+++ api.py
@@ -22,6 +22,5 @@ def quebrarCaptcha():
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
-    #app.run(debug=True, port=port, host='0.0.0.0')
     serve(app, port=port, host='0.0.0.0')
 
